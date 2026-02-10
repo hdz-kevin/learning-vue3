03a7e278f9f6ddebd83fabcccd78848364038145
hdz-kevin <kevinhdez592@gmail.com>
2026-02-10 17:57:24 -0600

    add & remove todos

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,4 +1,23 @@
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      todoTitle: "",
+      todos: [],
+    }
+  },
+
+  methods: {
+    addTodo() {
+      this.todos = [...this.todos, this.todoTitle];
+    },
+
+    removeTodo(todoTitle) {
+      this.todos = this.todos.filter(todo => todo !== todoTitle);
+    }
+  }
+};
+</script>
 
 <template>
   <nav class="navbar">
@@ -9,24 +28,18 @@
   <main class="container">
     <section>
       <form class="add-todo-form">
-        <input type="text" placeholder="Todo Title" />
+        <input v-model="todoTitle" type="text" placeholder="Todo Title" />
         <div>
-          <button>Add Todo</button>
+          <button @click.prevent="addTodo">Add Todo</button>
         </div>
       </form>
     </section>
 
     <section>
-      <div class="todo">
-        <p>Pasear al perro</p>
+      <div v-for="todo in todos" class="todo">
+        <p>{{ todo }}</p>
         <div>
-          <button class="remove-todo-btn">&times;</button>
-        </div>
-      </div>
-      <div class="todo">
-        <p>Pasear al perro</p>
-        <div>
-          <button class="remove-todo-btn">&times;</button>
+          <button @click="removeTodo(todo)" class="remove-todo-btn">&times;</button>
         </div>
       </div>
     </section>
