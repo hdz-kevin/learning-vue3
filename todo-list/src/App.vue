7ae9a0713f20cd0617a40ca30d9820d8bf53ab04
Tomas Hernandez <kevinhdez592@gmail.com>
2026-03-19 17:25:16 -0600

    refact App component to Composition API

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,118 +1,96 @@
-<script>
+<script setup>
 import AddTodoForm from "./components/AddTodoForm.vue";
 import Alert from "./components/Alert.vue";
 import Todo from "./components/Todo.vue";
 import Navbar from "./components/Navbar.vue";
-import Modal from "./components/Modal.vue";
-import Btn from "./components/Btn.vue";
 import axios from "axios";
 import Spinner from "./components/Spinner.vue";
 import EditTodoForm from "./components/EditTodoForm.vue";
+import { reactive, ref } from "vue";
 
-export default {
-  components: {
-    AddTodoForm,
-    Alert,
-    Btn,
-    EditTodoForm,
-    Modal,
-    Navbar,
-    Todo,
-    Spinner,
-  },
+const todos = ref([]);
+const isLoading = ref(false);
+const isPostingTodo = ref(false);
+const alert = reactive({
+  show: false,
+  message: "",
+  variant: "success",
+});
+const editTodoForm = reactive({
+  show: false,
+  todo: { id: null, title: null },
+});
 
-  data() {
-    return {
-      todos: [],
-      isLoading: false,
-      isPostingTodo: false,
-      alert: {
-        show: false,
-        message: "",
-        variant: "success",
-      },
-      editTodoForm: {
-        show: false,
-        todo: { id: null, title: null },
-      },
-    };
-  },
+// Fetch todos from API
+fetchTodos();
 
-  created() {
-    this.fetchTodos();
-  },
+function showAlert(message, variant = "success") {
+  alert.show = true;
+  alert.message = message;
+  alert.variant = variant;
+}
 
-  methods: {
-    async fetchTodos() {
-      this.isLoading = true;
-      try {
-        const res = await axios.get("/api/todos");
-        this.todos = res.data;
-      } catch (e) {
-        this.showAlert("Upps! Something went wrong to load todos", "danger");
-      }
-      this.isLoading = false;
-    },
+function showEditTodoForm(todo) {
+  editTodoForm.show = true;
+  editTodoForm.todo = { ...todo };
+}
 
-    async addTodo(title) {
-      if (title.trim() === "") {
-        this.showAlert("Title cannot be empty", "danger");
-        return;
-      }
+async function fetchTodos() {
+  isLoading.value = true;
+  try {
+    const res = await axios.get("/api/todos");
+    todos.value = res.data;
+  } catch (e) {
+    showAlert("Upps! Something went wrong to load todos", "danger");
+  }
+  isLoading.value = false;
+}
 
-      this.isPostingTodo = true;
-      try {
-        const res = await axios.post("/api/todos", { title });
+async function addTodo(title) {
+  if (title.trim() === "") {
+    showAlert("Title cannot be empty", "danger");
+    return;
+  }
 
-        // this.fetchTodos();
-        // Update UI manually on client side to avoid extra request.
-        // Important: do it if the request is successful
-        this.todos.push(res.data);
-      } catch (e) {
-        this.showAlert(
-          "Failed to add todo, check your internet connection",
-          "danger",
-        );
-      }
-      this.isPostingTodo = false;
-    },
+  isPostingTodo.value = true;
+  try {
+    const res = await axios.post("/api/todos", { title });
 
-    async removeTodo(id) {
-      await axios.delete(`/api/todos/${id}`);
+    // this.fetchTodos();
+    // Update UI manually on client side to avoid extra request.
+    // Important: do it if the request is successful
+    todos.value.push(res.data);
+  } catch (e) {
+    showAlert("Failed to add todo, check your internet connection", "danger");
+  }
+  isPostingTodo.value = false;
+}
 
-      // Update UI on client side
-      this.todos = this.todos.filter((todo) => todo.id !== id);
-    },
+async function removeTodo(id) {
+  await axios.delete(`/api/todos/${id}`);
 
-    showEditTodoForm(todo) {
-      this.editTodoForm.show = true;
-      this.editTodoForm.todo = { ...todo };
-    },
+  // Update UI on client side
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+}
 
-    async updateTodo() {
-      const { id, title } =  this.editTodoForm.todo;
+async function updateTodo() {
+  const { id, title } = editTodoForm.todo;
 
-      if (title.trim() === "") {
-        this.showAlert("Title cannot be empty", "danger");
-        return;
-      }
+  if (title.trim() === "") {
+    showAlert("Title cannot be empty", "danger");
+    return;
+  }
 
-      try {
-        await axios.put(`/api/todos/${id}`, { title });
-        // Update on client side
-        this.todos.find((todo) => todo.id == id).title = title;
-      } catch (e) {
-        this.showAlert("Failed to editing todo", "danger");
-      }
+  try {
+    await axios.put(`/api/todos/${id}`, { title });
+    // Update on client side
+    todos.value.find((todo) => todo.id == id).title = title;
+  } catch (e) {
+    showAlert("Failed to editing todo", "danger");
+  }
 
-      this.editTodoForm.show = false;
-    },
-
-    showAlert(message, variant = "success") {
-      this.alert = { show: true, message, variant };
-    },
-  },
-};
+  editTodoForm.show = false;
+}
 </script>
 
 <template>
